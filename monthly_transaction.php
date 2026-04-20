d008e3544c22c7ecec189f1b126f8dd8ab580805
iwacheto <ivailo@webrika.bg>
2026-04-20 14:50:37 +0300

    Add store records into MonthlyTransactionCron table

--- monthly_transaction.php
+++ monthly_transaction.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('display_errors', 1); error_reporting(E_ALL);
+// ini_set('display_errors', 1); error_reporting(E_ALL);
 
 require_once __DIR__ . '/cron_bootstrap.php';
 require_once __DIR__ . '/modules/Contacts/cron/MonthlyTransactionCron.php';
