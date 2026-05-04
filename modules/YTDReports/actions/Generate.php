4a51430c3977b4e84556114828508fc96e14ec8f
iwacheto <ivailo@webrika.bg>
2026-05-04 16:12:13 +0300

    Try to resolve error, returning from Generate action in YTD Report

--- modules/YTDReports/actions/Generate.php
+++ modules/YTDReports/actions/Generate.php
@@ -13,40 +13,37 @@ class YTDReports_Generate_Action extends Vtiger_Action_Controller
 
     public function process(Vtiger_Request $request)
     {
-        $contactId = $request->get('contact_id');
-
         $response = new Vtiger_Response();
 
-        if (!$contactId) {
-            $response->setError('Missing contact_id');
-            $response->emit();
-            return;
+        try {
+            $contactId = $request->get('contact_id');
+
+            if (!$contactId) {
+                throw new Exception('Missing contact_id');
+            }
+
+            $contactRecord = Vtiger_Record_Model::getInstanceById($contactId, 'Contacts');
+            $client_id = $contactRecord->get('cf_898');
+
+            if (!$client_id) {
+                throw new Exception('Client ID not found');
+            }
+
+            $date_range = Contacts_CronHelpers::buildMonthlyDateRange();
+
+            $activityService = new Contacts_ActivitySummaryService();
+            $activityService->generateAndStoreForClient(strval($client_id), $date_range);
+
+            $holdingsService = new Contacts_StatementOfHoldingsService();
+            $holdingsService->processClient(strval($client_id), $date_range);
+
+            $response->setResult([
+                'success' => true,
+                'message' => 'YTD Reports generated successfully'
+            ]);
+        } catch (Throwable $e) {
+            $response->setError($e->getMessage());
         }
-
-        // Get client_id from contact
-        $contactRecord = Vtiger_Record_Model::getInstanceById($contactId, 'Contacts');
-        $client_id = $contactRecord->get('cf_898');
-
-        if (!$client_id) {
-            $response->setError('Client ID not found');
-            $response->emit();
-            return;
-        }
-
-        // Build date range (previous month)
-        $date_range = Contacts_CronHelpers::buildMonthlyDateRange();
-
-        // Run your services (same as cron)
-        $activityService = new Contacts_ActivitySummaryService();
-        $activityService->generateAndStoreForClient(strval($client_id), $date_range);
-
-        $holdingsService = new Contacts_StatementOfHoldingsService();
-        $holdingsService->processClient(strval($client_id), $date_range);
-
-        $response->setResult([
-            'success' => true,
-            'message' => 'YTD Reports generated successfully'
-        ]);
 
         $response->emit();
     }
